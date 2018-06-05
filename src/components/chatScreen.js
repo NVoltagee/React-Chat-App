7fa9f975f41aec204abfc3fb8953312184480126
Norman Trujillo <natrujillo9@gmail.com>
2018-06-05 15:54:14 -0400

    Added Chatkit Manager to authenticate users.

--- src/components/chatScreen.js
+++ src/components/chatScreen.js
@@ -1,6 +1,30 @@
 import React from "react";
+import Chatkit from "@pusher/chatkit";
 
 class ChatScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: {}
+    };
+  }
+
+  componentDidMount() {
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator: "v1:us1:6d9a326a-188f-46de-a497-785d16179cba",
+      userId: this.props.currentUsername,
+      tokenProvider: new Chatkit.TokenProvider({
+        url: "http://localhost:3001/authenticate"
+      })
+    });
+
+    chatManager
+      .connect()
+      .then(currentUser => {
+        this.setState({ currentUser });
+      })
+      .catch(error => console.error("error", error));
+  }
   render() {
     return (
       <div>
