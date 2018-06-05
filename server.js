e1997c6ad56cff89af74cb71945080a4dd2e97a9
Norman Trujillo <natrujillo9@gmail.com>
2018-06-05 14:51:16 -0400

    Added Chatkit SDK.

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const Chatkit = require("@pusher/chatkit-server");
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(cors())
+const chatkit = new Chatkit.default({
+  instanceLocator: "v1:us1:6d9a326a-188f-46de-a497-785d16179cba",
+  key:
+    "5677de03-d46d-489e-ad90-429e974cbea7:6JAsZMSwqQgLoHjPw7uwrMH0n0dh+nuYauT/EBzCQXY="
+});
 
-const PORT = 3001
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+const PORT = 3001;
 app.listen(PORT, err => {
   if (err) {
-    console.error(err)
+    console.error(err);
   } else {
-    console.log(`Running on port ${PORT}`)
+    console.log(`Running on port ${PORT}`);
   }
-})
+});
