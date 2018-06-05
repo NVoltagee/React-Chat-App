a6b3d3f7648b5de63e5c852bed31ce4161e20bcc
Norman Trujillo <natrujillo9@gmail.com>
2018-06-05 15:36:29 -0400

    Added create user using Chatkit SDK.

--- server.js
+++ server.js
@@ -15,6 +15,24 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
+app.post("/users", (req, res) => {
+  const { username } = req.body;
+
+  chatkit
+    .createUser({
+      name: username,
+      id: username
+    })
+    .then(() => res.sendStatus(201))
+    .catch(error => {
+      if (error.error_type === "services/chatkit/user_already_exists") {
+        res.sendStatus(200);
+      } else {
+        res.status(error.statusCode).json(error);
+      }
+    });
+});
+
 const PORT = 3001;
 app.listen(PORT, err => {
   if (err) {
