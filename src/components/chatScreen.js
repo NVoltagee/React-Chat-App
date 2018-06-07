fea3d066dc34b1ec867ab6dd03628a0c281e0147
Norman Trujillo <natrujillo9@gmail.com>
2018-06-07 14:46:10 -0400

    Subscribed to room.

--- src/components/chatScreen.js
+++ src/components/chatScreen.js
@@ -1,7 +1,17 @@
 import React from "react";
 import Chatkit from "@pusher/chatkit";
+import MessageList from "./messageList";
 
 class ChatScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: {},
+      currentRoom: {},
+      messages: []
+    };
+  }
+
   componentDidMount() {
     const chatManager = new Chatkit.ChatManager({
       instanceLocator: "v1:us1:6d9a326a-188f-46de-a497-785d16179cba",
@@ -13,13 +23,65 @@ class ChatScreen extends React.Component {
 
     chatManager
       .connect()
-      .then(currentUser => console.log("currentUser", currentUser))
-      .catch(error => console.error(error));
+      .then(currentUser => {
+        this.setState({ currentUser });
+        return currentUser.subscribeToRoom({
+          roomId: 8963375,
+          messageLimit: 100,
+          hooks: {
+            onNewMessage: message => {
+              this.setState({
+                messages: [...this.state.messages, message]
+              });
+            }
+          }
+        });
+      })
+      .then(currentRoom => {
+        this.setState({ currentRoom });
+      })
+      .catch(error => console.error("error", error));
   }
+
   render() {
+    const styles = {
+      container: {
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column"
+      },
+      chatContainer: {
+        display: "flex",
+        flex: 1
+      },
+      whosOnlineListContainer: {
+        width: "300px",
+        flex: "none",
+        padding: 20,
+        backgroundColor: "#2c303b",
+        color: "white"
+      },
+      chatListContainer: {
+        padding: 20,
+        width: "85%",
+        display: "flex",
+        flexDirection: "column"
+      }
+    };
+
     return (
-      <div>
-        <h1>Chat</h1>
+      <div style={styles.container}>
+        <div style={styles.chatContainer}>
+          <aside style={styles.whosOnlineListContainer}>
+            <h2>Online People</h2>
+          </aside>
+          <section style={styles.chatListContainer}>
+            <MessageList
+              messages={this.state.messages}
+              style={styles.chatList}
+            />
+          </section>
+        </div>
       </div>
     );
   }
